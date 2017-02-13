aed5c1ac5a5001682568f3897f5459f5fff00140
melaniesdavis <melaniesdavis@hotmail.com>
2017-02-13 17:46:14 -0500

    Created project.

---
+++ src/PersonalBestFitness_MVC/PersonalBestFitness_MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PersonalBestFitness_MVC.Startup))]
+namespace PersonalBestFitness_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
